005878b6db0b58bff123b27da3e35e25f497cde8
Karina Barros <kbarros.o@hotmail.com>
2024-09-11 16:26:29 -0300

    add metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,23 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas:",
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked // mesmo que: meta.checked != true
+    })
+    if(abertas.length == 0) {
+        console.log("Não há metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas abertas:" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -82,6 +97,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -99,6 +118,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima.")
